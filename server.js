564c2df9f77121fa6c95750f383832f8a86b8c05
Rizki Nasution <rizkirnasution@gmail.com>
2022-10-20 09:59:00 +0700

    refactor: changed server

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(
 );
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/img", express.static("./public"));
+app.use("/img", express.static("./public/photo"));
 
 // root router
 app.get("/", (req, res) =>
