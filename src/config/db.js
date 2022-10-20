c6c9774c4cc3450647de568e97114dc8aab83629
Rizki Nasution <rizkirnasution@gmail.com>
2022-10-20 08:13:10 +0700

    refactor: changed env to production.

--- src/config/db.js
+++ src/config/db.js
@@ -1,19 +1,19 @@
 const { Pool } = require("pg");
 const {
   NODE_ENV,
-  DB_HOST,
-  DB_USER,
-  DB_PASSWORD,
-  DB_NAME,
-  DB_PORT,
+  PGHOST,
+  PGUSER,
+  PGPASSWORD,
+  PGDATABASE,
+  PGPORT,
 } = require("../utils/env");
 
 const config = {
-  host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  database: DB_NAME,
-  port: DB_PORT,
+  host: PGHOST,
+  user: PGUSER,
+  password: PGPASSWORD,
+  database: PGDATABASE,
+  port: PGPORT,
 };
 
 if (NODE_ENV === "production") {
--- src/utils/env.js
+++ src/utils/env.js
@@ -8,11 +8,12 @@ module.exports = {
   CLIENT_URL: process.env.CLIENT_URL,
 
   // database
-  DB_HOST: process.env.DB_HOST,
-  DB_USER: process.env.DB_USER,
-  DB_PASSWORD: process.env.DB_PASSWORD,
-  DB_NAME: process.env.DB_NAME,
-  DB_PORT: process.env.DB_PORT,
+  PGHOST: process.env.PGHOST,
+  PGUSER: process.env.PGUSER,
+  PGPASSWORD: process.env.PGPASSWORD,
+  PGDATABASE: process.env.PGDATABASE,
+  PGPORT: process.env.PGPORT,
+  DATABASE_URL : process.env.DATABASE_URL,
 
   // jwt
   JWT_SECRET: process.env.JWT_SECRET,
